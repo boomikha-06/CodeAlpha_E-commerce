700867c16629d5b21b87b70eb498bb5d8ed9117f
boomikha-06 <boomishanthi42@gmail.com>
2026-04-17 22:45:02 +0530

    Add CartItem model for shopping cart functionality

---
+++ cartitem.py
@@ -0,0 +1,5 @@
+class CartItem(models.Model):
+    cart_item_id = models.AutoField(primary_key=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
