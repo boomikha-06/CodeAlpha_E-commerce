9ea01eaa8649817c7a5df378dc6cf5a30a942004
boomikha-06 <boomishanthi42@gmail.com>
2026-04-17 22:46:19 +0530

    Create OrderItem model for order management

---
+++ orderitem.py
@@ -0,0 +1,6 @@
+class OrderItem(models.Model):
+    order_item_id = models.AutoField(primary_key=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
