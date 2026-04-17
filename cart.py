b9ff99afcb8d971c8e6780799a946025fb77496e
boomikha-06 <boomishanthi42@gmail.com>
2026-04-17 22:43:25 +0530

    Add Cart model with user and timestamp fields

---
+++ cart.py
@@ -0,0 +1,4 @@
+class Cart(models.Model):
+    cart_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
